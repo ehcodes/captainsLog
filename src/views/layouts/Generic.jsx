dbbc8148ac843b8dafb10cd2edf55c75f3461892
ehcodes <engrid.hernandez@gmail.com>
2023-10-02 11:55:46 -0400

    negligible change in whitespace

--- src/views/layouts/Generic.jsx
+++ src/views/layouts/Generic.jsx
@@ -2,15 +2,15 @@ const React = require("react");
 
 const Generic = ({ title, children }) => {
   return (
-      <html>
-        <head>
-          <title>{title}</title>
-        </head>
-        <body>
-          <h1>{title}</h1>
-          {children}
-        </body>
-      </html>
+    <html>
+      <head>
+        <title>{title}</title>
+      </head>
+      <body>
+        <h1>{title}</h1>
+        {children}
+      </body>
+    </html>
   );
 };
 
